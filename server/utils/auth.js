e9359239da8a36c8aa24c949184a154fed0c5a69
Jemel Williams <jwilliams25400@yahoo.com>
2021-11-11 19:53:21 -0500

    saving updates

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  // TODO: we only need the input parameter 'req'
-  // authMiddleware: function (req, res, next) {
-  authMiddleware: function ( { req } ) {
+  authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -18,9 +16,7 @@ module.exports = {
     }
 
     if (!token) {
-      // TODO: this should return req not throwing error
-      return req;
-      // return res.status(400).json({ message: 'You have no token!' });
+      return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -29,14 +25,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      // TODO: we will return req at the end of this function when encountering error in  deciphering token
-      // return res.status(400).json({ message: 'invalid token!' });
+      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // TODO: we will return req instead of send to next endpoint
     // send to next endpoint
-    // next();
-    return req;
+    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
